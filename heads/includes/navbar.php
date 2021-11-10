6395598a8830f89a22c42973d65232dd039400e7
hpmouton <hubertthedev@gmail.com>
2021-11-10 11:26:39 -0800

    Initial Commit

---
+++ heads/includes/navbar.php
@@ -0,0 +1,59 @@
+
+<div class="header">
+		<div class="header-left">
+			<div class="menu-icon dw dw-menu"></div>
+			<div class="search-toggle-icon dw dw-search2" data-toggle="header_search"></div>
+			
+		</div>
+		<div class="header-right">
+			<div class="user-notification">
+				<div class="dropdown">
+					<a class="dropdown-toggle no-arrow" href="#" role="button" data-toggle="dropdown">
+					<i class="fas fa-bell">				<span class="badge badge-danger">4</span>
+</i></a>
+					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+							<ul>
+								<?php
+								  $query = mysqli_query($conn,"select * from tblemployees where role = 'Staff' and Department = '$session_depart' ORDER BY tblemployees.emp_id desc limit 4") or die(mysqli_error());
+		                         while ($row = mysqli_fetch_array($query)) {
+		                         $id = $row['emp_id'];
+		                             ?>
+
+								<li class="d-flex align-items-center justify-content-between">
+									
+										
+										<div class="txt">
+											<div class="font-14 weight-600"><?php echo $row['FirstName'] . " " . $row['LastName']; ?></div>
+											<div class="font-12 weight-500" data-color="#b2b1b6"><?php echo $row['Dob']; ?></div>
+										</div>
+									
+								</li>
+								<?php }?>
+							</ul>
+					</div>
+				</div>
+			</div>
+			
+			<div class="user-info-dropdown">
+				<div class="dropdown">
+
+					<?php $query= mysqli_query($conn,"select * from tblemployees where emp_id = '$session_id'")or die(mysqli_error());
+								$row = mysqli_fetch_array($query);
+						?>
+
+					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+						<span class="user-icon">
+							<img src="<?php echo (!empty($row['location'])) ? '../uploads/'.$row['location'] : '../uploads/NO-IMAGE-AVAILABLE.jpg'; ?>" alt="">
+						</span>
+						<span class="user-name"><?php echo $row['FirstName']. " " .$row['LastName']; ?></span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+						<a class="dropdown-item" href="head_profile.php"><i class="dw dw-user1"></i> Profile</a>
+						<a class="dropdown-item" href="change_password.php"><i class="dw dw-help"></i> Reset Password</a>
+						<a class="dropdown-item" href="../logout.php"><i class="dw dw-logout"></i> Log Out</a>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
---
+++ vendors/scripts/datagraph.js
@@ -0,0 +1,236 @@
+var options = {
+    series: [{
+            name: "Staff On Leave",
+            data: [10, 0, 12, 6, 8, 6, 4, 2, 20, 25, 22, 30]
+        },
+
+
+    ],
+    chart: {
+        height: 300,
+        type: 'area',
+        zoom: {
+            enabled: false,
+        },
+        dropShadow: {
+            enabled: true,
+            color: '#000',
+            top: 18,
+            left: 7,
+            blur: 16,
+            opacity: 0.2
+        },
+        toolbar: {
+            show: false
+        }
+    },
+    colors: ['#f0746c', '#255cd3'],
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        width: [3, 3],
+        curve: 'smooth'
+    },
+    grid: {
+        show: false,
+    },
+    markers: {
+        colors: ['#f0746c', '#255cd3'],
+        size: 5,
+        strokeColors: '#ffffff',
+        strokeWidth: 2,
+        hover: {
+            sizeOffset: 2
+        }
+    },
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        labels: {
+            style: {
+                colors: '#8c9094'
+            }
+        }
+    },
+    yaxis: {
+        min: 0,
+        max: 35,
+        labels: {
+            style: {
+                colors: '#8c9094'
+            }
+        }
+    },
+    legend: {
+        position: 'top',
+        horizontalAlign: 'right',
+        floating: true,
+        offsetY: 0,
+        labels: {
+            useSeriesColors: true
+        },
+        markers: {
+            width: 10,
+            height: 10,
+        }
+    }
+};
+
+var options2 = {
+    series: [70, 40],
+    chart: {
+        height: 200,
+        type: 'donut',
+    },
+    colors: ['#003049', '#d62828', '#f77f00', '#fcbf49', '#e76f51'],
+    plotOptions: {
+        radialBar: {
+            dataLabels: {
+                name: {
+                    fontSize: '22px',
+                },
+                value: {
+                    fontSize: '16px',
+                },
+                total: {
+                    show: true,
+                    label: 'Total',
+                    formatter: function(w) {
+                        return 260
+                    }
+                }
+            }
+        }
+    },
+    labels: ['Foreign', 'Namibian'],
+};
+
+var options3 = {
+    series: [100, 10],
+    chart: {
+        height: 200,
+        type: 'donut',
+    },
+    colors: ['#003049', '#d62828', '#f77f00', '#fcbf49', '#e76f51'],
+    plotOptions: {
+        radialBar: {
+            dataLabels: {
+                name: {
+                    fontSize: '22px',
+                },
+                value: {
+                    fontSize: '16px',
+                },
+                total: {
+                    show: true,
+                    label: 'Total',
+                    formatter: function(w) {
+                        return 260
+                    }
+                }
+            }
+        }
+    },
+    labels: ['Full-Time', 'Part-Time'],
+};
+
+var options5 = {
+    series: [50, 60],
+    chart: {
+        height: 200,
+        type: 'donut',
+    },
+    colors: ['#003049', '#d62828', '#f77f00', '#fcbf49', '#e76f51'],
+    plotOptions: {
+        radialBar: {
+            dataLabels: {
+                name: {
+                    fontSize: '22px',
+                },
+                value: {
+                    fontSize: '16px',
+                },
+                total: {
+                    show: true,
+                    label: 'Total',
+                    formatter: function(w) {
+                        return 260
+                    }
+                }
+            }
+        }
+    },
+    labels: ['Males', 'Females'],
+};
+
+var options6 = {
+    series: [1, 2],
+    chart: {
+        height: 200,
+        type: 'donut',
+    },
+    colors: ['#003049', '#d62828', '#f77f00', '#fcbf49', '#e76f51'],
+    plotOptions: {
+        radialBar: {
+            dataLabels: {
+                name: {
+                    fontSize: '22px',
+                },
+                value: {
+                    fontSize: '16px',
+                },
+                total: {
+                    show: true,
+                    label: 'Total',
+                    formatter: function(w) {
+                        return 260
+                    }
+                }
+            }
+        }
+    },
+    labels: ['Medical Leave', 'Casual Leave'],
+};
+
+var chart = new ApexCharts(document.querySelector("#activities-chart"), options);
+chart.render();
+
+var chart2 = new ApexCharts(document.querySelector("#appointment-chart"), options2);
+chart2.render();
+
+var chart3 = new ApexCharts(document.querySelector("#surgery-chart"), options3);
+chart3.render();
+
+var chart4 = new ApexCharts(document.querySelector("#application-chart"), options5);
+chart4.render();
+
+var leaveChart = new ApexCharts(document.querySelector("#leave-chart"), options6);
+leaveChart.render();
+// datatable init
+$('document').ready(function() {
+    $('.data-table').DataTable({
+        scrollCollapse: false,
+        autoWidth: false,
+        responsive: true,
+        searching: false,
+        bLengthChange: false,
+        bPaginate: true,
+        bInfo: false,
+        columnDefs: [{
+            targets: "datatable-nosort",
+            orderable: false,
+        }],
+        "lengthMenu": [
+            [5, 25, 50, -1],
+            [5, 25, 50, "All"]
+        ],
+        "language": {
+            "info": "_START_-_END_ of _TOTAL_ entries",
+            searchPlaceholder: "Search",
+            paginate: {
+                next: '<i class="ion-chevron-right"></i>',
+                previous: '<i class="ion-chevron-left"></i>'
+            }
+        },
+    });
+});
